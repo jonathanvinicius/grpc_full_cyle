7cb254bac2a317bd389c2c8f7b1b63266ebb6d74
Jonathan Vinicius Braz Silva <jonathan.braz@compasso.com.br>
2023-10-09 15:51:58 -0300

    feat: create method createCategoryStreamBidirectional

--- internal/service/category.go
+++ internal/service/category.go
@@ -95,3 +95,31 @@ func (c *CategoryService) CreateCategoryStream(stream pb.CategoryService_CreateC
 		})
 	}
 }
+
+func (c *CategoryService) CreateCategoryStreamBidirectional(stream pb.CategoryService_CreateCategoryStreamBidirectionalServer) error {
+	for {
+		category, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		categoryResult, err := c.CategoryDB.Create(category.Name, category.Description)
+		if err != nil {
+			return err
+		}
+
+		err = stream.Send(&pb.Category{
+			Id:          categoryResult.ID,
+			Name:        category.Name,
+			Description: category.Name,
+		})
+
+		if err != nil {
+			return err
+		}
+	}
+}
